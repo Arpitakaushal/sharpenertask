db9b2548aab2896859e8e901056bbb604cb9e2d1
ArpitaKaushal <arpitakaushal538@gmail.com>
2024-04-04 15:24:50 +0530

    filter function

--- task9/index.js
+++ task9/index.js
@@ -1,17 +1,36 @@
+let input=document.createElement('input')
+input.type="text"
+input.id="description"
+input.fontweight="italic"
+let form=document.querySelector('form')
+let btn=document.querySelector('button')
+form.insertBefore(input,btn)
+console.log(input)
+
+let fruit=document.querySelector(".fruits")
+form.addEventListener("submit",(event)=>{
+  event.preventDefault()
+  let li=document.createElement('li')
+  fruitadd=document.querySelector('#fruit-to-add')
+  li.className="fruit"
+  li.innerHTML=fruitadd.value+`<p style='font-style:italic;'>${input.value}</p><button class="delete-btn">x</button>`
+ fruit.appendChild(li)
+
+})
 let filter=document.querySelector('#filter')
 
-filter.addEventListener('keyup',(e)=>{
-    let text=e.target.value.toLowerCase()
-    let fruititem=document.querySelectorAll('.fruit')
-   for(let i=0;i<fruititem.length;i++)
-   {
-    let curr=(fruititem[i].firstChild.textContent.toLowerCase())
-    if(curr.indexOf(text)==-1)
-    {
-        fruititem[i].style.display='none'
-    }
-    else{
-        fruititem[i].style.display='flex'
-    }
-   }
+filter.addEventListener('keyup',(event)=>{
+ let textenter=event.target.value.toLowerCase()
+ let fruit=document.querySelectorAll('.fruit')
+ for(let i=0;i<fruit.length;i++)
+ {
+  let currfruit=fruit[i].firstChild.textContent.toLowerCase()
+  if(currfruit.indexOf(textenter)===-1)
+  {
+     fruit[i].style.display="none"
+  }
+  else{
+    fruit[i].style.display="flex"
+  }
+ }
 })
